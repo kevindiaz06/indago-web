73fe243c9ae78c1607258537c9ee7370b195b2d1
kevin diaz <diazkevinmota@gmail.com>
2026-03-16 13:32:23 +0100

    feat: Add DatabaseSeeder to create an admin user.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'kevindiazmota@gmail.com',
+            'password' => '123456789',
         ]);
     }
 }
